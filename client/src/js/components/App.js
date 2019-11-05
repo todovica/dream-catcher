3864bbe62062c19d47e3b5b90c134b90a5c695da
todovica <ana.todovic@gmail.com>
2019-11-05 11:27:47 +0100

    add some routing and some misty effect

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -10,12 +10,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    height: '350px',
-    backgroundImage: `url(${backgroundImage})`,
+    height: '250px',
+    backgroundImage: `-webkit-gradient(linear, left top, left bottom, color-stop(0%, rgba(0, 0, 0, 0)), color-stop(59%, rgba(0, 0, 0, 0)), color-stop(10%, #f5f0f0)), -webkit-gradient(linear, left top, left bottom, color-stop(10%, rgba(0, 0, 0, 0)), color-stop(70%, rgba(0, 0, 0, 0)), color-stop(190%, #f5f0f0)), url(${backgroundImage})`,
     marginBottom: '20px'
   },
   titleFont: {
@@ -24,7 +25,7 @@ const useStyles = makeStyles(theme => ({
     padding: '20px',
     color: '#000067',
     fontFamily: 'cursive',
-    
+
   },
 }));
 
@@ -36,15 +37,19 @@ function App() {
              direction="row"
              justify="space-between"
              alignItems="flex-start" className={classes.root} >
-               <Typography variant="h2" component="h6" className={classes.titleFont} > {'Share your dream'} </Typography>
+               <Box>
+                 <Typography variant="h2" component="h6" className={classes.titleFont} > {'Dream catcher'} </Typography>
+                 <Typography variant="h4" component="h4" className={classes.titleFont} > {'Share your dream'} </Typography>
+               </Box>
                <AddNewDream />
            </Grid>
 
            <Container>
              <BrowserRouter>  
                <Switch>
-                 <Route exact path='/confirm/:id' component={ConfirmPage} />
-                 <Route exact path='/' component={List} />
+                 <Route exact path='/dream-catcher/confirm/:id' component={ConfirmPage} />
+                 <Route exact path='/dream-catcher' component={List} />
+                 <Redirect from='*' to='/dream-catcher'/>
                  
                </Switch>
              </BrowserRouter>
